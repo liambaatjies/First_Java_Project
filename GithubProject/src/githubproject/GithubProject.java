3b73c4f504c9908d87f2a5ac63155bb7b5984abb
liamb <liamb@DESKTOP-IKD1MBL>
2026-02-18 11:47:52 +0200

    My initial commit and added print statements

---
+++ GithubProject/src/githubproject/GithubProject.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package githubproject;
+
+
+public class GithubProject {
+/* This is a 
+    multi-line 
+    comment
+    */
+    //string is an array of characrters
+    // john
+    //[j][o][h][n]
+    
+   
+    public static void main(String[] args) {
+      System.out.println("My name is John");
+       System.out.print("I am a funny");
+       System.out.println(" Clown");
+       System.out.println("This has been fun!");
+    }
+    
+}
